0f63c03ffd2ab19568767d613393c445a9717960
Shivansh Sharma <shivansh1145@gmail.com>
2021-08-15 18:42:39 +0530

    final commit

---
+++ models/all.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+
+const databSchema = new mongoose.Schema({
+    Date: Date,
+    payment: String
+});
+
+const Datab = mongoose.model("Datab", databSchema);
+
+module.exports = Datab;
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Transaction = require("./transaction");
+
+
+const customrSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email:
+    {
+        type: String,
+        required: true
+    },
+    Balance: Number,
+    transactions:[{
+        type: mongoose.Schema.Types.ObjectId, ref:"Transaction"}],
+    avatar:String,
+    contact:Number,
+    about:String
+});
+
+const Customer = mongoose.model("Customer", customrSchema);
+
+module.exports = Customer;
